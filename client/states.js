0eb235270b14ab9ca224e24ee1137adff70641c9
Maksym Tabachuk <maksym.tabachuk@flex.com>
2018-01-11 05:33:54 -0800

    [Studio] Delete screen "app.listviewcool"

--- client/states.js
+++ client/states.js
@@ -69,14 +69,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.listviewcool', {
-        views: {
-            app: {
-                controller: 'app_listviewcool',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.listviewcool');
-                }
-            }
-        }
     });
 });
